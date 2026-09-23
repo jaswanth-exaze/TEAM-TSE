6191bcd4b7f0d59105550c961ce46a07eba045bb
Ruthwik <ruthvik.madhavarapu@exazeit.com>
2026-09-23 17:16:35 +0530

    Add project images and enhance projects page layout with new styles and interactivity

---
+++ team/members/ruthwik/projects/script.js
@@ -0,0 +1,12 @@
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
+
+btn1.addEventListener("click", () => {
+
+    window.open("./odin-recipes/recipesPrj.html", "_blank")
+})
+
+btn2.addEventListener("click", () => {
+
+    window.open("./Landing Pages/landingPagesPrj.html");
+})
